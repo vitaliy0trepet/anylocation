770afa4701d1b0bc78da9a47eb3beb651d1136cd
vitaliy0trepet <48066529+vitaliy0trepet@users.noreply.github.com>
2021-05-15 18:59:26 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
 var hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu')
+    menu = document.querySelector('.menu');
 
 hamburger.onclick = function(event) {
     hamburger.classList.toggle('opened')
     menu.classList.toggle('opened')
 }
 
+var positions = document.querySelectorAll('.positions');
+positions.forEach(function(parent) {
+	var child = parent.querySelector('.position')
+	for(var i=1;i<4;i++){
+		var clone = child.cloneNode(true);
+		parent.append(clone)
+	}
+})
